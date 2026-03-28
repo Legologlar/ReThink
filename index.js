590f28000b57ee62489b620573f9fc35a4f61c0e
Legologlar <reisoglugorkem@mayaokullari.k12.tr>
2026-03-28 15:36:16 +0300

    Implement points retrieval and addition endpoints
    Added endpoints to retrieve and update user points in the database.

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require("express");
-const app = express();
+const { Pool } = require("pg");
 
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// test
 app.get("/", (req, res) => {
   res.send("Backend çalışıyor");
+});
+
+// puan getir
+app.get("/points", async (req, res) => {
+  const result = await pool.query("SELECT points FROM users WHERE id = 1");
+  res.json(result.rows[0]);
+});
+
+// puan ekle
+app.get("/add-points", async (req, res) => {
+  await pool.query("UPDATE users SET points = points + 10 WHERE id = 1");
+  res.send("puan eklendi");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server çalışıyor");
-});
-
-let points = 0;
-
-app.get("/points", (req, res) => {
-  res.json({ points });
-});
-
-app.get("/add-points", (req, res) => {
-  points += 10;
-  res.json({ message: "puan eklendi", points });
 });
